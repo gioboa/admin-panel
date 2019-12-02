44adc054a810c613d110ec6bf831ef9d887b561c
gioboa <giorgiob.boa@gmail.com>
2019-12-02 22:14:06 +0100

    refactor: change name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,6 +20,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('trade-manager app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('admin-panel app is running!');
   });
 });
